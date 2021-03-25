fa9d8a6445c9ba622645b13d1445c197a3cfe6db
lebonq <57987762+lebonq@users.noreply.github.com>
2021-03-25 18:45:41 +0100

    Sapling cannot grow on path Fixes #11

---
+++ src/main/java/fr/lebon/autopath/mixin/PathBlockSapling.java
@@ -0,0 +1,18 @@
+package fr.lebon.autopath.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import fr.lebon.autopath.AutoPath;
+import net.minecraft.block.Block;
+import net.minecraft.world.gen.feature.Feature;
+
+@Mixin(Feature.class) 
+public class PathBlockSapling {
+	@Inject(method = "isSoil(Lnet/minecraft/block/Block;)Z", cancellable = true, at = @At(value = "RETURN"))
+    private static void makePathSaplingable(Block block, CallbackInfoReturnable<Boolean> cir) {
+        if (!cir.getReturnValue() && block.is(AutoPath.PATH)) cir.setReturnValue(true);
+    }
+}
