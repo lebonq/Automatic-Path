796091e79295e81c6c3534edc7b65ec5563b5158
lebonq <57987762+lebonq@users.noreply.github.com>
2021-03-26 21:10:37 +0100

    Items are making path Fixes #17

--- src/main/java/fr/lebon/autopath/mixin/BlockOnStepped.java
+++ src/main/java/fr/lebon/autopath/mixin/BlockOnStepped.java
@@ -9,6 +9,7 @@ import fr.lebon.autopath.AutoPath;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
@@ -16,7 +17,7 @@ import net.minecraft.world.World;
 public class BlockOnStepped {
 	@Inject(method = "onSteppedOn", cancellable = true, at = @At(value = "HEAD"))
     private void transformGrassToPathWhenSteppedOn(World world, BlockPos pos, Entity entity,CallbackInfo cir) {
-        if(world.getBlockState(pos).isOf(Blocks.GRASS_BLOCK)){ //select grass
+        if(world.getBlockState(pos).isOf(Blocks.GRASS_BLOCK) && (entity instanceof LivingEntity)){ //select grass and if entity is a mob or player
             world.setBlockState(pos,AutoPath.PATH_BLOCK.getDefaultState()); //On remplace par un block path
             cir.cancel();
         }
